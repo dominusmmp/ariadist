1c44da671ae6d305939c51bb31c2f212b4bd25c5
Dominus MMP <59709069+dominusmmp@users.noreply.github.com>
2021-09-25 16:26:14 +0330

    +

--- auopush.sh
+++ auopush.sh
@@ -1,13 +1,15 @@
 git add .
 
-echo 'Enter the commit message:'
-read commitMessage
+# echo 'Enter the commit message:'
+# read commitMessage
 
-git commit -m "$commitMessage"
+# git commit -m "$commitMessage"
+git commit -m "+"
 
-echo 'Enter the name of the branch:'
-read branch
+# echo 'Enter the name of the branch:'
+# read branch
 
-git push origin $branch
+# git push origin $branch
+git push origin master
 
 read
